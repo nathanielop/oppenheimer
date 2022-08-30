ec34daec50867ed2c73451f2f0b48edc216d58cb
Nathaniel Pendy <nathanielp2402@gmail.com>
2022-08-30 13:43:23 -0500

    Initial (basically just the default Rust hello world)

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use std::fs;
+
+fn main() {
+   const f = fs::read_dir(__dir)
+}
---
+++ target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/nathanielpendy/Desktop/hello-rust/target/debug/hello-rust: /Users/nathanielpendy/Desktop/hello-rust/src/main.rs
